89f671b45def8ce116c8b761d1e452a056f3e82a
AdamZsoldos <adam.zsoldos@reddwarfgames.com>
2022-01-31 09:23:23 +0100

    Added streams.Song, SongService

---
+++ src/main/java/streams/Song.java
@@ -0,0 +1,35 @@
+package streams;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Song {
+
+    private final String title;
+    private final int length;
+    private final List<String> performers;
+    private final LocalDate release;
+
+    public Song(String title, int length, List<String> performers, LocalDate release) {
+        this.title = title;
+        this.length = length;
+        this.performers = performers;
+        this.release = release;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public List<String> getPerformers() {
+        return performers;
+    }
+
+    public LocalDate getRelease() {
+        return release;
+    }
+}
